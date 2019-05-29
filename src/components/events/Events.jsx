fd3cd4d0ef4d6471c3e70eb1f350a331de3f4162
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-05-29 10:01:51 -0500

    Added news and events list components

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
+import { Grid, Paper } from "@material-ui/core";
+import EventItem from "./EventItem";
 
 export default class Events extends Component {
+  makeEvent = events =>
+    events.map(event => <EventItem key={event.id} event={event} />);
+
   render() {
-    return <div>Hello from events</div>;
+    return (
+      <Paper>
+        <h2>Events near you:</h2>
+        <div className="eventContainer">
+          {this.makeEvent(this.props.events)}
+        </div>
+      </Paper>
+    );
   }
 }
--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import { Grid, Paper } from "@material-ui/core";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component {
+  makeNews = news => news.map(item => <NewsItem key={item.id} item={item} />);
+
   render() {
-    return <div>Hello from news</div>;
+    return (
+      <Paper>
+        <h2>Breaking News:</h2>
+        <div className="newsContainer">{this.makeNews(this.props.news)}</div>
+      </Paper>
+    );
   }
 }

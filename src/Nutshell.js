394e362508cfe1bbb98ed572983ed8f4f354f703
Deep Patel <deep.ag123007@gmail.com>
2019-05-30 09:11:29 -0500

    remove friends is working

--- src/Nutshell.js
+++ src/Nutshell.js
@@ -1,66 +1,98 @@
 import React, { Component } from "react";
-import { withRouter } from 'react-router'
+import { withRouter } from "react-router";
 import Navbar from "./components/nav/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import ApplicationViews from "./components/ApplicationViews";
-import API from "./modules/dbCalls"
+import API from "./modules/dbCalls";
 
 class Nutshell extends Component {
   state = {
-    isUserLoggedIn: true
+    isUserLoggedIn: true,
+    friends: []
+  };
+  ///////////////////////////////// start Friends Area //////////////////////////////////////
+  componentDidMount() {
+    const loggedInUser = sessionStorage.getItem("activeUser");
+    const newState = {};
+
+    API.getFriendsList(loggedInUser, "true", "true")
+      .then(friends => {
+        newState.friends = friends;
+      })
+
+      .then(() => this.setState(newState));
+  }
+
+  deleteFriend = (id, friendId) => {
+    const newState = {};
+    API.deleteFriend(id, friendId)
+      .then(API.getFriendsList(id, "true", "true"))
+      .then(friends => {
+        newState.friends = friends;
+      })
+      .then(() => {
+        this.setState(newState);
+      });
   };
 
+  /////////////////////////////////// End Friends Area //////////////////////////////////////
   login = (username, password) => {
     API.loginUser(username, password).then(user => {
       if (user.length === 0) {
-        alert("username and email do not match")
+        alert("username and email do not match");
       } else {
-        sessionStorage.setItem("activeUser", user[0].id)
-        this.setState({ isUserLoggedIn: true })
-        this.props.history.push('/')
+        sessionStorage.setItem("activeUser", user[0].id);
+        this.setState({ isUserLoggedIn: true });
+        this.props.history.push("/");
       }
-
-    })
-  }
+    });
+  };
 
   register = (username, email, password) => {
     const newUser = {
       username: username,
       email: email,
       password: password
-    }
+    };
 
     API.getAllUsers().then(users => {
-      if (users.find(user =>
-        username === user.username || email === user.email
-      ) !== undefined) {
-        alert("You are already a user")
+      if (
+        users.find(
+          user => username === user.username || email === user.email
+        ) !== undefined
+      ) {
+        alert("You are already a user");
       } else {
         API.addUser(newUser).then(newuserInfo => {
-          this.login(newuserInfo.username, newuserInfo.password)
-          this.props.history.push('/')
-        })
+          this.login(newuserInfo.username, newuserInfo.password);
+          this.props.history.push("/");
+        });
       }
-    })
-
-  }
+    });
+  };
 
   logout = () => {
-    sessionStorage.removeItem("activeUser")
-    this.props.history.push('/login')
-  }
+    sessionStorage.removeItem("activeUser");
+    this.props.history.push("/login");
+  };
 
   render() {
     return (
       <>
-        <Navbar loggedIn={this.state.isUserLoggedIn}
-          logout={this.logout} />
-        <Sidebar loggedIn={this.state.isUserLoggedIn} />
-        <ApplicationViews loggedIn={this.state.isUserLoggedIn}
-          login={this.login} register={this.register} />
+        <Navbar loggedIn={this.state.isUserLoggedIn} logout={this.logout} />
+        <Sidebar
+          loggedIn={this.state.isUserLoggedIn}
+          friends={this.state.friends}
+          deleteFriend={this.deleteFriend}
+        />
+        <ApplicationViews
+          loggedIn={this.state.isUserLoggedIn}
+          login={this.login}
+          register={this.register}
+        />
       </>
     );
   }
 }
 
-export default withRouter(Nutshell)
+export default withRouter(Nutshell);
--- src/components/friends/Friends.jsx
+++ src/components/friends/Friends.jsx
@@ -1,7 +1,19 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import { Grid, Paper } from "@material-ui/core";
+// import FriendItem from "./FriendItem";
 
-export default class Friends extends Component {
-  render() {
-    return <div />;
-  }
-}
+// export default class Friends extends Component {
+//   showFriend = friends =>
+//     friends.map(item => <FriendItem key={item.id} item={item} />);
+
+//   render() {
+//     return (
+//       <Paper>
+//         <h2>Friends:</h2>
+//         <div className="friendContainer">
+//           {this.showFriend(this.props.friends)}
+//         </div>
+//       </Paper>
+//     );
+//   }
+// }
